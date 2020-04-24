aed0966ed0cf7c6ed1b0d2d3995e8e8680f45ed7
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-04-24 11:39:22 -0400

    tests tweaks

--- tests/test_divvy_simple.py
+++ tests/test_divvy_simple.py
@@ -1,42 +1,44 @@
 import divvy
-import yacman
+from yacman import YacAttMap
 import os
 # For interactive debugging:
 # import logmuse
 # logmuse.init_logger("divvy", "DEBUG")
 
 
-def test_activate_package():
-	dcc = divvy.ComputingConfiguration()
-	dcc
-	dcc.activate_package("default")
-	t = dcc.compute.submission_template
-	t2 = dcc["compute"]["submission_template"]
-	assert(t == t2)
-	dcc.activate_package("slurm")
-	t = dcc.compute.submission_template
-	t2 = dcc["compute"]["submission_template"]
-	assert(t == t2)
+class TestPackageaAtivation:
+    def test_activate_package(self):
+        dcc = divvy.ComputingConfiguration()
+        dcc.activate_package("default")
+        t = dcc.compute.submission_template
+        t2 = dcc["compute"]["submission_template"]
+        assert(t == t2)
+        dcc.activate_package("slurm")
+        t = dcc.compute.submission_template
+        t2 = dcc["compute"]["submission_template"]
+        assert(t == t2)
 
-def test_write_script():
-	dcc = divvy.ComputingConfiguration()
-	dcc
-	dcc.activate_package("singularity_slurm")
-	extra_vars = {
-		"singularity_image": "simg",
-		"jobname": "jbname",
-		"code": "mycode"
-		}
-	dcc.write_script("test.sub", extra_vars)
-	with open("test.sub", 'r') as f:
-		contents = f.read()
-		assert(contents.find("mycode")>0)
-		assert(contents.find("{SINGULARITY_ARGS}") <0)
-	os.remove("test.sub")
+
+class TestWriting:
+    def test_write_script(self):
+        dcc = divvy.ComputingConfiguration()
+        dcc
+        dcc.activate_package("singularity_slurm")
+        extra_vars = {
+            "singularity_image": "simg",
+            "jobname": "jbname",
+            "code": "mycode"
+        }
+        dcc.write_script("test.sub", extra_vars)
+        with open("test.sub", 'r') as f:
+            contents = f.read()
+        assert(contents.find("mycode") > 0)
+        assert(contents.find("{SINGULARITY_ARGS}") < 0)
+        os.remove("test.sub")
+
 
 class TestAdapters:
     def test_write_script_adapters(self):
-        from yacman import YacAttMap
         dcc = divvy.ComputingConfiguration()
         dcc.adapters = {"MEM": "compute.mem"}
         dcc.activate_package("singularity_slurm")
@@ -51,7 +53,6 @@ class TestAdapters:
         os.remove("test.sub")
 
     def test_adapters_overwitten_by_others(self):
-        from yacman import YacAttMap
         dcc = divvy.ComputingConfiguration()
         dcc.adapters = {"MEM": "compute.mem"}
         dcc.activate_package("singularity_slurm")
@@ -60,12 +61,14 @@ class TestAdapters:
             "MEM": 333,
             "compute": compute
         }
-        dcc.write_script("test.sub", extra_vars)
-        with open("test.sub", 'r') as f:
+        dcc.write_script("test1.sub", extra_vars)
+        with open("test1.sub", 'r') as f:
             contents = f.read()
+
             assert not (contents.find("1000") > 0)
             assert (contents.find("333") > 0)
-        os.remove("test.sub")
+        print(contents)
+        os.remove("test1.sub")
 
 # def test_update():
 # 	# probably will be removed later
